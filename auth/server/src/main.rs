0a810c52cfde1f88893768f7ce527ce6a679a0ee
Mauricio Neira <mauro@up.codes>
2021-12-10 00:21:50 -0800

    Auth get signature message

--- auth/server/src/main.rs
+++ auth/server/src/main.rs
@@ -1,10 +1,19 @@
 use actix_web::{get, middleware::Logger, post, web, App, HttpResponse, HttpServer, Responder};
 
+mod signature_message;
+mod utils;
+
+/// Returns the message a user must sign with their private key to authenticate.
 #[get("/signature_message/{public_key}")]
 async fn get_signature_message(web::Path(public_key): web::Path<String>) -> impl Responder {
-    HttpResponse::Ok().body(public_key)
+    let nonce = utils::generate_nonce();
+    let issued_at = utils::get_current_time();
+    let signature_message =
+        signature_message::get_strangemood_signature_message(nonce, issued_at, public_key);
+    HttpResponse::Ok().body(signature_message)
 }
 
+// TODO: implement the login
 #[post("/echo")]
 async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
