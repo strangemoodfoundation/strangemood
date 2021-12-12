f612c43613f354e17c686bc12e016da884a68e24
nader <nader@brev.dev>
2021-12-12 19:19:02 +0000

    post request takes omgbody. struct needs building out

--- metadata/src/main.rs
+++ metadata/src/main.rs
@@ -1,3 +1,4 @@
+use serde::{Serialize, Deserialize};
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
 #[actix_web::main]
@@ -14,16 +15,27 @@ async fn main() -> std::io::Result<()> {
 }
 
 #[get("/listing/{pubkey}")]
+// Get listing metadata (no need for auth)
 async fn getlisting(path: web::Path<String>) -> impl Responder {
     HttpResponse::Ok().body(format!("Get Listing. your pubkey: {}!", &path))
 }
 
+// Login with public private key auth 
 #[post("/login/{pubkey}")]
-async fn postlogin(path: web::Path<String>) -> impl Responder {
-    HttpResponse::Ok().body(format!("Post Login. your pubkey: {}!", &path))
+async fn postlogin(path: web::Path<String>, omgbody: web::Json<LoginPostBody>) -> impl Responder {
+    // HttpResponse::Ok().body("hello world")
+    HttpResponse::Ok().body(format!("Post Login. your pubkey: {}! your json version {}", &path, omgbody.version))
 }
 
+// Update listing metadata; should use keypair auth + take OMG as post body
 #[post("/listing/{pubkey}")]
 async fn postlisting(path: web::Path<String>) -> impl Responder {
     HttpResponse::Ok().body(format!("Post Listing. your pubkey: {}!", &path))
+}
+
+/// Struct to receive user input.
+#[derive(Serialize, Deserialize)]
+pub struct LoginPostBody {
+    pub version: String
+    // TODO: finish building out the omg spec
 }
