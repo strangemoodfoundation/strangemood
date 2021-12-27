d62bf0e2d57ac08ed5a73d291771b8f6b87ea2e3
Evan Conrad <evan@roomservice.dev>
2021-12-26 17:58:54 -0800

    wip metadata

--- services/src/rpc.rs
+++ services/src/rpc.rs
@@ -1,5 +1,6 @@
 use std::str::FromStr;
 
+use anchor_lang::AccountDeserialize;
 use anyhow::Result;
 use bs58;
 use jsonrpc::{self, Error};
@@ -9,18 +10,18 @@ use solana_sdk::pubkey::Pubkey;
 use strangemood;
 use uuid::Uuid;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 struct EncodingParams {
     // "base58"
     encoding: String,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 struct BlockContext {
     slot: i64,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 struct AccountValue {
     data: [String; 2],
     executable: bool,
@@ -29,13 +30,13 @@ struct AccountValue {
     rentEpoch: i64,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 struct AccountInfoResponse {
     context: BlockContext,
     value: AccountValue,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 struct AccountInfo<T> {
     data: T,
     executable: bool,
@@ -44,7 +45,7 @@ struct AccountInfo<T> {
     rentEpoch: i64,
 }
 
-fn get_listing_info(url: &str, pubkey: Pubkey) -> Result<()> {
+fn get_listing_info(url: &str, pubkey: Pubkey) -> Result<Listing> {
     let client = jsonrpc::client::Client::simple_http(url, None, None)?;
 
     let response = client.send_request(jsonrpc::Request {
@@ -55,7 +56,7 @@ fn get_listing_info(url: &str, pubkey: Pubkey) -> Result<()> {
                 encoding: "base58".to_string(),
             })?,
         ],
-        id: json!(2),
+        id: serde_json::to_value::<i32>(2)?,
         jsonrpc: Some("2.0"),
     })?;
 
@@ -67,8 +68,11 @@ fn get_listing_info(url: &str, pubkey: Pubkey) -> Result<()> {
         None => return Err(anyhow::format_err!("Oh no")),
     };
 
-    let ownerPubkey = Pubkey::from_str(info.value.owner.as_str())?;
-    bs58::decode(info.value.data);
+    let ownerPubkey = Pubkey::from_str(info.clone().value.owner.as_str())?;
 
-    Ok(())
+    let mut data: &[u8] = &bs58::decode(info.value.data[0].clone()).into_vec()?;
+    // let mut u8s = &mut data.iter().map(|c| *c as u8).collect::<Vec<_>>();
+
+    let listing = strangemood::Listing::try_deserialize(&mut data)?;
+    Ok(listing)
 }
