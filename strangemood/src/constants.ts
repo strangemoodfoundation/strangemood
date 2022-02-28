d89afb1dd0c45f9ed0e6dd3a9313729aa829ad6f
Evan Conrad <evan@roomservice.dev>
2022-02-28 13:27:20 -0800

    add charter to constants

--- strangemood/src/constants.ts
+++ strangemood/src/constants.ts
@@ -4,11 +4,15 @@ export type NET = typeof MAINNET;
 
 export type Environment = {
   strangemood_program_id: anchor.web3.PublicKey;
+  strangemood_foundation_charter: anchor.web3.PublicKey;
 };
 
 export const MAINNET: Environment = {
   strangemood_program_id: new anchor.web3.PublicKey(
     "sm3L2zgBxMgz34U5f2zifjMDFYEZNEc1SNC6Ur8CXWx"
+  ),
+  strangemood_foundation_charter: new anchor.web3.PublicKey(
+    "D8hxZ192cEtaWBYTQfpLKDtHPUMjd8AVkAAVTdLyLujx"
   ),
 };
 
@@ -16,10 +20,16 @@ export const TESTNET: Environment = {
   strangemood_program_id: new anchor.web3.PublicKey(
     "sm3L2zgBxMgz34U5f2zifjMDFYEZNEc1SNC6Ur8CXWx"
   ),
+  strangemood_foundation_charter: new anchor.web3.PublicKey(
+    "D8hxZ192cEtaWBYTQfpLKDtHPUMjd8AVkAAVTdLyLujx"
+  ),
 };
 
 export const DEVNET: Environment = {
   strangemood_program_id: new anchor.web3.PublicKey(
     "sm3L2zgBxMgz34U5f2zifjMDFYEZNEc1SNC6Ur8CXWx"
+  ),
+  strangemood_foundation_charter: new anchor.web3.PublicKey(
+    "D8hxZ192cEtaWBYTQfpLKDtHPUMjd8AVkAAVTdLyLujx"
   ),
 };
