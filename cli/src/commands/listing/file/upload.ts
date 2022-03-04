961057532a9c1ac9db9e0c5f663b963fe2f7321e
Jacob Peddicord <jacob.peddicord@hey.com>
2022-03-04 12:00:36 -0500

    clean up upload command

--- cli/src/commands/listing/file/upload.ts
+++ cli/src/commands/listing/file/upload.ts
@@ -2,29 +2,12 @@ import { Command, Flags } from "@oclif/core";
 import ora from "ora";
 import { packToFs } from 'ipfs-car/pack/fs';
 import { FsBlockStore } from 'ipfs-car/blockstore/fs'
-import { TreewalkCarSplitter } from 'carbites/treewalk';
-import { CarIndexedReader } from '@ipld/car'
-import { postCar } from '../../../postCar';
+import { uploadCars, encryptFile, splitCar, uploadKey } from '../../../file';
 import { sep } from 'path';
 import { tmpdir } from 'os';
-import { mkdtemp } from 'fs';
-import fs from 'fs'
-import { execSync } from "child_process";
+import fs, { mkdtemp } from 'fs';
 import { getProgram } from "../../../provider";
-import fetch from 'node-fetch';
 import path from 'path';
-
-const PRECRYPT_ENDPOINT = "https://api.precrypt.org";
-
-async function make_temp_dir(): Promise<string> {
-  const tmpDir = tmpdir();
-  return new Promise((resolve, reject) => {
-    mkdtemp(`${tmpDir}${sep}`, (err, directory) => {
-      if (err) reject(err);
-      resolve(directory);
-    });
-  });
-}
 
 export default class UploadFile extends Command {
   static description = "Upload associated files to IPFS with optional encryption";
@@ -42,6 +25,7 @@ export default class UploadFile extends Command {
       char: 'e',
       description: "Encrypt the file before uploading to IPFS using precrypt. Users will only be able to decrypt the file if they purchase the listing.",
       required: false,
+      default: false
     })
   };
 
@@ -60,111 +44,59 @@ export default class UploadFile extends Command {
 
   async run(): Promise<void> {
     const { args, flags } = await this.parse(UploadFile);
-    const spinner = ora("Connecting").start();
-    const temp_dir = await make_temp_dir();
+    const tmpDir = tmpdir();
+    const tempDir: string = fs.mkdtempSync(`${tmpDir}${sep}`);
 
     try {
       var inputPath = args["path"];
       const extension = path.extname(inputPath).replace(".", "");
-      const outputPath = `${temp_dir}/output.car`;
-      const fileKeyPath = `${temp_dir}/file.json`;
-      const cipherPath = `${temp_dir}/cipher.bin`;
-      const recryptKeyPath = `${temp_dir}/recrypt.json`;
+      const outputPath = `${tempDir}/output.car`;
+      const fileKeyPath = `${tempDir}/file.json`;
+      const cipherPath = `${tempDir}/cipher.bin`;
+      const recryptKeyPath = `${tempDir}/recrypt.json`;
+
 
       if (flags.encrypt) {
-        spinner.text = "Encrypting file with precrypt...";
-        try {
-          execSync(`precrypt keygen ${fileKeyPath}`);
-          execSync(`precrypt encrypt ${inputPath}  ${fileKeyPath}  ${recryptKeyPath} ${cipherPath}`);
-          inputPath = cipherPath;
-        } catch (err) {
-          console.log("The following error was thrown running precrypt.");
-          console.log("Did you install the precrypt cli with 'npm i -g precrypt'?");
-          throw err;
-        }
+        ora.promise(
+          encryptFile(inputPath, fileKeyPath, recryptKeyPath, cipherPath),
+          "Encrypting file with precrypt"
+        );
       }
 
-      spinner.text = "Packing file to CAR...";
+      const carSpinner = ora("Packing file to CAR...");
       const { root } = await packToFs({ input: inputPath, output: outputPath, blockstore: new FsBlockStore(), wrapWithDirectory: false });
-      var stats = fs.statSync(outputPath)
-      var fileSizeInBytes = stats.size;
+      const rootCID = root.toString();
+      carSpinner.succeed("File packed to CAR");
 
-      const reader = await CarIndexedReader.fromFile(outputPath);
-      const [rootCid] = await reader.getRoots();
-      const targetSize = 100000000; // chunk to ~100MB CARs
-      const num_cars = Math.ceil(fileSizeInBytes / targetSize);
-      const splitter = new TreewalkCarSplitter(reader, targetSize);
+      const spinner = ora(`Splitting CAR into chunks...`);
+      const carPaths = await splitCar(outputPath, tempDir);
+      spinner.succeed(`CAR split into ${carPaths.length} chunks`);
 
-      var promises: Promise<any>[] = []
-      var i = 0;
-      spinner.text = `Uploading CAR chunk: ${num_cars - i} remaining...`;
-      for await (const car of splitter.cars()) {
-        const chunks = []
-        for await (const chunk of car) {
-          chunks.push(chunk)
-        }
-        const bytes = new Uint8Array([].concat(...chunks.map(c => Array.from(c))));
 
-        const promise = postCar(bytes);
-        promise.then(() => {
-          i += 1;
-          spinner.text = `Uploading CAR chunk: ${num_cars - i} remaining...`;
-        });
-        promises.push(promise);
-      }
-      await reader.close();
-
-      const responses = await Promise.all(promises);
-      responses.forEach((json) => {
-        if (!json || json['cid'] !== rootCid.toString()) {
-          this.log("Error uploading the file, servers may be busy")
-        }
-      });
+      const uploadSpinner = ora(`Uploading CAR chunk: ${carPaths.length} remaining...`);
+      await uploadCars(carPaths, rootCID);
+      uploadSpinner.succeed(`Uploaded ${carPaths.length} chunks`)
 
       if (flags.encrypt) {
-        const spinner = ora("Connecting").start();
+        const keySpinner = ora("Connecting...").start();
 
-        spinner.text = "Fetching listing mint";
+        keySpinner.text = "Fetching listing mint";
         const program = await getProgram({
           net: flags.cluster as any,
         });
         const listing = await program.account.listing.fetch(args["listing"]);
-        spinner.text = `Uploading recryption key to precrypt node...`;
-        const recryptionKeys = JSON.parse(fs.readFileSync(recryptKeyPath).toString());
-        const body = {
-          "recryption_keys": recryptionKeys,
-          "mint": listing.mint.toString(),
-          "file_cid": root.toString(),
-          "file_extension": extension
-        };
-        const body_str = JSON.stringify(body);
-        try {
-          const resp = await fetch(`${PRECRYPT_ENDPOINT}/key/store`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: body_str
-          });
-          const json = await resp.json();
-          let keyCID = json['cid'];
-          spinner.stop();
-          console.log(`Precrypt key CID: ${keyCID}`);
-        } catch (err) {
-          console.log("Error uploading recryption key to precrypt node:");
-          throw err;
-        }
-      }
 
-      spinner.stop();
-      this.log(`File CID: ${root}`);
+        keySpinner.text = `Uploading recryption key to precrypt node...`;
+        const keyCID = await uploadKey(recryptKeyPath, listing.mint.toString(), rootCID, extension);
+        keySpinner.succeed("Uploaded recryption key to precrypt node");
+        console.log(`Precrypt key CID: ${keyCID}`);
+      }
+      this.log(`File CID: ${rootCID}`);
     } catch (err) {
       console.log(err);
     } finally {
-      // Stop spinner if spinning
-      if (spinner.isSpinning) spinner.stop()
       // Clean temp dir
-      fs.rmSync(temp_dir, { recursive: true });
+      fs.rmSync(tempDir, { recursive: true });
     }
   }
 }
---
+++ cli/src/file.ts
@@ -0,0 +1,143 @@
+import { CarIndexedReader } from '@ipld/car/indexed-reader';
+import fetch from 'node-fetch';
+import { execSync } from 'node:child_process';
+import fs from 'fs';
+import { TreewalkCarSplitter } from 'carbites';
+
+const PRECRYPT_ENDPOINT = "https://api.precrypt.org";
+
+export async function encryptFile(inputPath: string, fileKeyPath: string, recryptKeyPath: string, cipherPath: string) {
+   try {
+      execSync(`precrypt keygen ${fileKeyPath}`).toString();
+      execSync(`precrypt encrypt ${inputPath}  ${fileKeyPath}  ${recryptKeyPath} ${cipherPath}`, { stdio: 'pipe' }).toString();
+      inputPath = cipherPath;
+   } catch (err) {
+      console.log(err);
+      console.log();
+      console.log("The above error was thrown running precrypt.");
+      console.log("Did you install the precrypt cli with 'npm i -g precrypt'?");
+      throw err;
+   }
+}
+
+export async function splitCar(filePath: string, tempDir: string): Promise<string[]> {
+   const reader = await CarIndexedReader.fromFile(filePath);
+   const targetSize = 100000000; // chunk to ~100MB CARs
+
+   // Get file size
+   var stats = fs.statSync(filePath)
+   var fileSizeInBytes = stats.size;
+   const numCars = Math.ceil(fileSizeInBytes / targetSize);
+   // spinner.text = `Splitting CAR into chunks: ${numCars - i} remaining...`;
+   const splitter = new TreewalkCarSplitter(reader, targetSize);
+
+   var i = 0;
+   var carPaths = [];
+   for await (const car of splitter.cars()) {
+      const chunks = []
+      for await (const chunk of car) {
+         chunks.push(chunk)
+      }
+      const bytes = new Uint8Array([].concat(...chunks.map(c => Array.from(c))));
+      // spinner.text = `Splitting CAR into chunks: ${numCars - i} remaining...`;
+      const carPath = `${tempDir}/car-${i}.car`;
+      fs.writeFileSync(carPath, bytes);
+      carPaths.push(carPath);
+      i++;
+   }
+   await reader.close();
+   return carPaths;
+}
+
+export async function uploadCars(carPaths: string[], rootCID: string) {
+   var i = 0;
+   var promises: Promise<any>[] = []
+   for (const path of carPaths) {
+      let bytes = fs.readFileSync(path);
+      const promise = postCar(bytes);
+      promise.then(() => {
+         i += 1;
+         // uploadSpinner.text = `Uploading CAR chunk: ${numCars - i} remaining...`;
+      });
+      promises.push(promise);
+   }
+
+   const responses = await Promise.all(promises);
+   responses.forEach((json) => {
+      if (!json || json['cid'] !== rootCID.toString()) {
+         this.log("Error uploading the file, servers may be busy")
+         throw "Failed to upload car"
+      }
+   });
+}
+
+class HTTPResponseError extends Error {
+   response: any;
+
+   constructor(response) {
+      super(`HTTP Error Response: ${response.status} ${response.statusText}`);
+      this.response = response;
+   }
+}
+
+const checkStatus = response => {
+   if (response.ok) {
+      return response;
+   } else {
+      throw new HTTPResponseError(response);
+   }
+}
+
+export async function postCar(bytes: Uint8Array): Promise<any> {
+   var retryCount = 0;
+   while (retryCount < 3) {
+      try {
+         const response = await fetch('https://web3proxy.fly.dev/api/web3/car', {
+            method: "post",
+            headers: {
+               "Content-Type": "application/car"
+            },
+            body: bytes
+         });
+
+         try {
+            checkStatus(response);
+            const json = await response.json();
+            return json;
+         } catch (error) {
+            // console.error(error);
+            // const errorBody = await error.response.text();
+            // console.error(`Error body: ${errorBody}`);
+         }
+      } catch (error) {
+         // console.log(error);
+      }
+      retryCount++;
+   }
+   return null;
+}
+
+export async function uploadKey(recryptKeyPath: string, mint: string, fileCID: string,  extension: string): Promise<string> {
+   const recryptionKeys = JSON.parse(fs.readFileSync(recryptKeyPath).toString());
+   const body = {
+      "recryption_keys": recryptionKeys,
+      "mint": mint,
+      "file_cid": fileCID,
+      "file_extension": extension
+   };
+   const body_str = JSON.stringify(body);
+   try {
+      const resp = await fetch(`${PRECRYPT_ENDPOINT}/key/store`, {
+         method: 'POST',
+         headers: {
+            'Content-Type': 'application/json'
+         },
+         body: body_str
+      });
+      const json = await resp.json();
+      return json['cid'];
+   } catch (err) {
+      console.log("Error uploading recryption key to precrypt node:");
+      throw err;
+   }
+}
